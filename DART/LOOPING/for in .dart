13552c361955f59f70be655becb0b234a4a7905f
Wisleydan <weisleyalmond@outlook.com>
2025-10-23 07:38:49 -0700

    Added DART folder contents as regular files

---
+++ DART/LOOPING/for in .dart
@@ -0,0 +1,7 @@
+void main()
+{
+  var lists1 = [10, 20, 30, 40, 50];
+  for (var i in lists1) {
+    print(i);
+  }
+}
---
+++ DART/numbers.dart
@@ -0,0 +1,14 @@
+void main(){ 
+    int age = 17;
+    if (age >= 18)
+    {
+      print("You qualify to be a Kenyan citizen");
+    }
+    else if (age == 17)
+    {
+      print("You are a year away from qualifying to be a Kenyan citizen");
+    }
+    else {
+      print("You DO NOT qualify to be a Kenyan citizen!");
+    }
+}
